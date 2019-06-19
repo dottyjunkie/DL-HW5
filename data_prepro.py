f542c6ba06003ca16e83a940560c7fab88c0c719
Thomas <thomaschou861205@gmail.com>
2019-06-19 18:57:19 +0800

    trim out 0~9

--- data_prepro.py
+++ data_prepro.py
@@ -10,7 +10,20 @@ arr = np.array(cont)
 y = arr[:, 0]
 # X = arr[:, 1:].reshape((112799, 28, 28))
 X = arr[:, 1:] / 255
-noisy_X = X + np.random.normal(0,0.25,(112799, 784))
+
+
+# trim 0~9
+y_trim = []
+X_trim = []
+for i in range(y.shape[0]):
+	if y[i] > 10:
+		y_trim.append(y[i])
+		X_trim.append(X[i, :])
+y_trim = np.array(y_trim)
+# print(y_trim.shape) # (86399, )
+X_trim = np.stack(X_trim, axis=0)
+# print(X_trim.shape) # (86399, 784)
+noisy_X = X_trim + np.random.normal(0,0.25,X_trim.shape)
 
 col = 5
 row = 5
@@ -18,8 +31,9 @@ f, a = plt.subplots(row, col, figsize=(col, row))
 for i in range(row):
 	for j in range(col):
 		a[i][j].imshow(noisy_X[i*row + j, :].reshape((28,28)).transpose(), cmap='gray')
+		# print(y_trim[i*row + j])
 plt.show()
 
 f = open('data.pickle', 'wb')
-pickle.dump((X, noisy_X, y), f)
+pickle.dump((X_trim, noisy_X, y_trim), f)
 f.close()
